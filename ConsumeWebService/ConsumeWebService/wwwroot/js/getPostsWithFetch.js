6c74b9dd25739a6b4a13d291be49c022cb3ba6f1
ulestrange <lestrange.una@itsligo.ie>
2019-03-26 21:54:37 +0000

    added some stuff which uses fetch

---
+++ ConsumeWebService/ConsumeWebService/wwwroot/js/getPostsWithFetch.js
@@ -0,0 +1,15 @@
+﻿
+async function getpostswithfetch() {
+    const app = document.getElementById('root');
+
+    const postID = document.getElementById("inputpostid").value;
+
+    const container = document.createElement('main');
+    container.setAttribute('class', 'container');
+
+    app.appendChild(container);
+
+    fetch("https://jsonplaceholder.typicode.com/Posts") //defaults to a get
+        .then((response) => response.json()) // convert response to json
+        .then((j) => (container.innerHTML = JSON.stringify(j)))
+}
---
+++ ConsumeWebService/ConsumeWebService/wwwroot/js/getPostsWithFetch2.js
@@ -0,0 +1,21 @@
+﻿
+async function getpostswithfetch2() {
+    const app = document.getElementById('root');
+
+    const postID = document.getElementById("inputpostid").value;
+
+    const container = document.createElement('main');
+    container.setAttribute('class', 'container');
+
+    app.appendChild(container);
+
+        try {
+            var response = await fetch("https://jsonplaceholder.typicode.com/Posts");
+            var j =  response.json()
+            container.innerHTML = JSON.stringify(j)
+        }
+        catch (err) {
+            container.innerHTML = "Error, can't load " + fname + " " + err
+        }
+
+}
