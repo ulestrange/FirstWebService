74bab13ec22329c4a4d876718412dc196a2849bc
ulestrange <lestrange.una@itsligo.ie>
2019-04-01 17:00:02 +0100

    JQuery Ajax funtion for posts now can handle errors

--- ConsumeWebService/ConsumeWebService/wwwroot/js/getPostsJQuery.js
+++ ConsumeWebService/ConsumeWebService/wwwroot/js/getPostsJQuery.js
@@ -9,13 +9,26 @@
 //    });
 //});
 
+
+
+// this function will make a get request
+// based on the postid from the input element
+// when it receives the JSON it is parsed automatically
+// and then it is put into the relavent HTML tags.
+// if there is a http error code that is handled
+
+
 $("#btngetposts").click(function () {
     var postid = inputpostid.value;
     $.ajax({
-        url: 'https://jsonplaceholder.typicode.com/Posts/' + postid
-    }).then(function (data) {
-        $('#postid').html(data.id);
-        $('#posttitle').html(data.title);
-        $('#postcontent').html(data.body);
+        url: 'https://jsonplaceholder.typicode.com/Posts/' + postid,
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Something went wrong!");
+        },
+        success: function (data) {
+            $('#postid').html(data.id);
+            $('#posttitle').html(data.title);
+            $('#postcontent').html(data.body);
+        }
     });
-});
+})
